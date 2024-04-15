da64da0dc936bf93e416d45f1dbbf56377bcaa6d
ALAATARAB <alaaproschool@gmail.com>
2024-04-15 22:08:25 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,8 @@ import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule,{rawBody:true,cors:true,bodyParser:true});
-  app.enableCors({origin:"*",methods:"*",allowedHeaders:"*",credentials:true})
-  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')))
+  app.enableCors({origin:"*",methods:"*",allowedHeaders:"*",credentials:true});
+  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
   app.listen(3000);
 }
 bootstrap();
