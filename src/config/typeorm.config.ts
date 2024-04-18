bcad1c59e9d024c790aeda1e6d3e1bde7f7b46c5
ALAATARAB <alaaproschool@gmail.com>
2024-04-18 20:31:28 +0300

    .

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   port: 5432,
   username: "postgres",
   password: "postgres",
-  database: "taskmanagement",
+  database: "ajirni",
   entities: [__dirname + "/../**/*.entity{.ts,.js}"],
   synchronize: true
 };
