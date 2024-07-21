32ddaa0aa2d5c0a37960880b068755128cea6fa9
Mohammed Alaa Tarabishe <92860206+ALAATARAB@users.noreply.github.com>
2024-07-21 15:10:10 +0300

    .

--- src/tool/pipes/createTool.pipe.ts
+++ src/tool/pipes/createTool.pipe.ts
@@ -1,9 +1,9 @@
 import { ArgumentMetadata, Injectable, PipeTransform } from "@nestjs/common";
-import { CreateToolDTO } from "../dto/create-tool.dto";
+import { CreateToolDto } from "../dto/create-tool.dto";
 
 @Injectable()
 export class CreateToolPipe implements PipeTransform {
-  transform(value: CreateToolDTO, metadata: ArgumentMetadata) {
+  transform(value: CreateToolDto, metadata: ArgumentMetadata) {
     value.price = +value.price;
     value.baseQuantity = +value.baseQuantity;
     return value;
